6cc23fbffeacf69e2ecfdd7e077480d686082b8f
ku6ryo <ku6ryo@gmail.com>
2021-05-20 02:59:15 +0900

    added storing wasm file in server

---
+++ compiler/README.md
@@ -0,0 +1,10 @@
+To start the server locally, please run the following commands.
+```
+yarn
+yarn dev
+```
+
+Editor is on http://localhost:8080/editor.html
+
+If compile is successfully done, artifact id is shown in an alert.
+Please copy the ID and access http://localhost:8080/artifacts/{id}.wasm or {id}.wat
--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -1,11 +1,17 @@
+import fs, { write } from "fs"
+import util from "util"
 import path from "path"
 import express from "express"
 import { compile, createFileSourceMap } from "./compiler"
 import { CompileError } from "./compiler"
+import { v4 as uuid } from "uuid"
+
+const writeFile = util.promisify(fs.writeFile)
 
 const app = express()
 app.use(express.text())
 app.use(express.static(path.join(__dirname, "public")))
+app.use("/artifacts", express.static(path.join(__dirname, "../artifacts")))
 
 app.use((req, __, next) => {
   console.log(`[${new Date()}] ${req.method} ${req.url}`)
@@ -16,7 +22,12 @@ app.post("/api/compile", async (req: express.Request, res: express.Response) => 
   const code = req.body as string
   try {
     const result = await compile(await createFileSourceMap(code))
+    const id = uuid()
+    await writeFile(path.join(__dirname, "../artifacts/", id + ".wasm"), result.wasm)
+    await writeFile(path.join(__dirname, "../artifacts/", id + ".wat"), result.wat)
+
     res.json({
+      id,
       wasm: {
         size: result.wasm.length,
       },
@@ -29,7 +40,9 @@ app.post("/api/compile", async (req: express.Request, res: express.Response) => 
       res.status(400).json({
         message: e.message
       })
+      return
     }
+    throw e
   }
 })
 
